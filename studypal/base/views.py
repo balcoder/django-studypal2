170a2495e5facb51d5850b58b7bffeb8d04e7f0a
Des Barrett <des.barrett@gmail.com>
2023-03-18 14:45:20 +0000

    added topics component

--- studypal/base/views.py
+++ studypal/base/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
@@ -68,15 +68,28 @@ def home(request):
         Q(description__icontains=q) 
         #Q(topic__host__icontains=q)
         )
+    
     room_count = rooms.count()
-
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages }
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method ==  'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+    context = {'room': room, 'room_messages':room_messages, 'participants': participants }
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
@@ -119,3 +132,15 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request,'base/delete.html', {'obj': room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not authorized to update room')
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request,'base/delete.html', {'obj': message})
